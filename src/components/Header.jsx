925b9855e6fe80b595135d8909ea8c06ae14fe5d
aryashree <@aryashri.713@gmail.com>
2024-07-25 23:51:15 +0545

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Header = () => {
+return (
+    <div className='header'>
+
+        <div className="logo montserrat">
+            Travel
+        </div>
+
+        <div className="options montserrat">
+            <div className='homesec'>
+
+            <a href='#'>Home</a>
+            <div className="line"></div>
+            </div>
+
+            <div className='aboutsec'>
+
+            <a href='#'>About</a>
+            <div className="line"></div>
+            </div>
+            <div className='popsec'>
+
+            <a href='#'>Popular</a>
+            <div className="line"></div>
+            </div>
+            <div className='exploresec'>
+
+            <a href='#'>Explore</a>
+            <div className="line"></div>
+            </div>
+            <div className='homesec'>
+                </div>
+
+            
+
+    
+        </div>
+    </div>
+)
+}
+
+export default Header
---
+++ src/components/Section.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const Section = () => {
+  return (
+    <div className='section'>
+
+      <div className="welcome poppins-semibold">Welcome To Travel</div>
+
+      <div className="explore poppins-bold ">Explore <br></br>The World</div>
+
+      <div className="des poppins-regular">Live the trips exploring the world, discover paradises,<br></br> islands, mountains and much more, get your trip now.</div>
+
+      
+
+        
+        <button className='start poppins-medium'>Start Your Journey    
+        
+
+        </button>
+        
+        
+
+      
+      
+    </div>
+  )
+}
+
+export default Section
